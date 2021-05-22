ce736b6d5a1fbb650c1632367f88f74a6213d986
Nathan Heller <naheller@gmail.com>
2021-05-22 16:09:09 -0400

    Disable purge all

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,13 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  // purge: {
-  //   mode: "all",
-  //   content: ["./src/**/*.html", "./src/**/*.js"],
-  // },
+  purge: {
+    // mode: "all",
+    content: ["./src/**/*.html", "./src/**/*.js"],
+    // options: {
+    //   safelist: ['token']
+    // }
+  },
   theme: {
     fontFamily: {
       serif: ["Vollkorn", "Georgia", "serif"],
